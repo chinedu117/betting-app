6896ab1316b77a2169a41273ac0910cdb5d97f5a
chinedu117 <chinedudimonyeka@gmail.com>
2024-12-27 17:09:48 +0100

    add swagger docs

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,14 @@ import routes from './routes';
 import { connectDB } from './config/database';
 import Logger from './shared/logger';
 import { requestErrorHandler } from './shared/utils/errors';
+import swaggerOptions from './swagger';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
 const app: Express = express();
 
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json(), cors());
 app.use(express.urlencoded({ extended: true }));
 connectDB();
